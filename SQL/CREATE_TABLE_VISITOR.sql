76c551b16739852e56df57dd951ca607dd64d276
justin <justingooley@icloud.com>
2017-12-06 23:13:04 -0700

    Changed tables visitors to visitor. Added test data insert script.

--- SQL/CREATE_TABLE_VISITOR.sql
+++ SQL/CREATE_TABLE_VISITOR.sql
@@ -1,9 +1,11 @@
-CREATE TABLE Visitors (
+CREATE TABLE Visitor (
     ID int IDENTITY(1,1),
-    IP_Address VARCHAR(36),
+    IP_ADDRESS VARCHAR(45),
     COUNTRY_NAME VARCHAR(41), -- based on the longest possible name according to google.
     REGION_NAME VARCHAR(41),
     CITY VARCHAR (41),
-    LATITUDE DECIMAL (9,6),
-    LONGITUDE DECIMAL (9,6)
+    LATITUDE DECIMAL (9,5),
+    LONGITUDE DECIMAL (9,5),
+    INSERT_DATE DATETIME,
+    UPDATE_DATE DATETIME NULL
 )
