9124ee6c94c206654b4cd3d9d35ead25f53c99a9
justin <justingooley@icloud.com>
2017-12-06 23:16:33 -0700

    New visitor model and context files.

--- VisitorBackEnd/Data/VisitorContext.cs
+++ VisitorBackEnd/Data/VisitorContext.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using VisitorBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
 namespace VisitorBackEnd.Data
 {
-    public class VisitorContext
+    public class VisitorContext : DbContext
     {
-        public VisitorContext()
+        public VisitorContext(DbContextOptions<VisitorContext> options) : base(options)
         {
+        }
+
+        public DbSet<Visitor> Visitors { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Visitor>().ToTable("Visitor");
         }
     }
 }
--- VisitorBackEnd/Models/Visitor.cs
+++ VisitorBackEnd/Models/Visitor.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace VisitorBackEnd.Models
 {
     public class Visitor
     {
-        public Visitor()
-        {
-        }
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int  Id { get; set; }
+        [Column("IP_ADDRESS")]
+        public string IpAddress { get; set; }
+        [Column("COUNTRY_NAME")]
+        public string CountryName { get; set; }
+        [Column("REGION_NAME")]
+        public string RegionName { get; set; }
+        public string City { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        [Column("INSERT_DATE")]
+        public DateTime InsertDate { get; set; }
+        [Column("UPDATE_DATE")]
+        public DateTime? UpdateDate { get; set; }
     }
 }
